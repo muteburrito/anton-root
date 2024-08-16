44997f3e52d2b4728915a37456dd392de04db699
Chinmay Kulkarni <kulkarnichinmay65@gmail.com>
2024-08-16 18:28:49 +0530

    Fixing issue in DSL compilation
    "Name property cannot be changed in terms of Root Project"

--- .teamcity/_self/anton.kt
+++ .teamcity/_self/anton.kt
@@ -4,7 +4,6 @@ import _self.subProjects.*
 import jetbrains.buildServer.configs.kotlin.*
 
 object Anton : Project({
-    name = "Anton"
     description = "This is the root Project"
 
     subProjects(Prod, Staging)
